3a2dfaf3bb24b704e4d1838418a939fd3119aeda
BSJ <00bsj@naver.com>
2024-04-29 12:36:40 +0900

    240429

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// JSX란 ? JavaScript에 XML을 추가 확장한 문법
+// html 태그 사용 시반드시 닫힘 태그가 존재해야 함
+// 컴퍼런트가 반환 될 때 태그는 한개 이어야 함. <></>
+// 자바스크립트 코드를 중괄호를 사용해 데이터 바인딩 할 수 있음
+// 조건부 연산자 : {}내에서 조건부 연산자 사용 가능
+// 조건부 렌더링 : && 연산자의 특징을 활용해서 조건에 따라 화면 렌더링
+// 인라인 스타일링 :
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const name = "리액트";
+  const styleC = {
+    backgroundColor: "#222",
+    color: "royalblue",
+    fontSize: "2em",
+  };
+  return <p style={styleC}>{name === "리액트" && <h1>리액트 입니다.</h1>}</p>;
 }
 
 export default App;
